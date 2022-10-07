ebf17a7c52b80ad7898f62bfe14a4081c5d41167
Eric German <german.eric@gmail.com>
2022-10-07 18:58:46 +0200

    refactorisation traitement value

--- pycobol/zonage.py
+++ pycobol/zonage.py
@@ -34,61 +34,49 @@ class Zone:
          pic = ' '.join(t_ligne[debpic:finpic +1])
          nature_ = Nature_pic(pic)
          nature = nature_.nature
-
-   
-            
-         tabv = re.findall(r'\(\d+\)', pic)
-         long = -1
-         flag_par = 0
-         if tabv:
-            for i, item in enumerate(tabv):
-                item = item.translate(str.maketrans({'(':'' ,')':'' }))
-                tabv[i]  = item
-                flag_par = 1
-            long = int(tabv[0])    
-         else :
-            long = len(pic)
-         if pic[0] == 'S' and flag_par == 0:
-            long -= 1
-         if nature[-5:] == 'FLOAT': 
-              
-                if flag_par == 0 :
-                    long -= 1
-                else:    
-                    tab_ = re.search(r'V(9+)' ,pic)
-                    if tab_:
-                        long += len(tab_[1])
-                         
+         nature_obj = nature_
+         long = nature_obj.longueur                  
          return nature, pic, long
         
 
     def traite_value(t_ligne):
-         debval = -1
-         finval = -1
-         for cp, val in  enumerate(t_ligne) :
+        ''' Traitement des values dans la working storage
+            :param str tableau mot de la ligne
+            >>> ligne =   ['77', 'MAZONE', 'PIC', 'X(10)', 'VALUE', 'SPACE']
+            >>> Zone.traite_value(ligne)
+            ('', 'STR')
+            >>> ligne =   ['77', 'MAZONE', 'PIC', 'X(10)', 'VALUE', "'er'"]
+            >>> Zone.traite_value(ligne)
+            ('er', 'STR')
+        '''    
+
+        debval = -1
+        finval = -1
+        dico_ = {'ZERO'  : (0, 'NUM'), 
+                  'ZEROS' : (0, 'NUM'),
+                  'SPACE' : ('', 'STR'),
+                  'SPACES': ('', 'STR'),
+        }
+        value = ''
+        for cp, val in  enumerate(t_ligne) :
             if val == 'VALUE' :
                  nature_value = 'STR'
                  value = ' '.join(t_ligne[debval:])
-                 
-                 value_ = value.translate(str.maketrans({"\'":'' ,'\"':'' }))
-                 if value_ != value:
-                     nature_value = 'STR'
-                 elif value_ == 'ZERO':     
-                    value_ = 0
-                    nature_value = 'NUM'
-                 elif value_ == 'SPACE':     
-                    value_ = ''
-                    nature_value = 'STR'  
-                 elif value_ == 'SPACES':     
-                    value_ = ''
-                    nature_value = 'STR'  
-                    
-                 else:
-                     value_ = int(value)
-                     nature_value  = 'NUM'
-                 return value_,nature_value
-         else: 
-            return None, None
+                 break
+        else: 
+            return None, None         
+        value_ = value.translate(str.maketrans({"\'":'' ,'\"':'' }))
+        if value_ != value:
+            nature_value = 'STR'
+        else:    
+            try: 
+                #print('try', value_)
+                (value_ , nature_value) = dico_[value_] 
+            except: 
+                value_ = int(value)
+                nature_value  = 'NUM'
+            
+        return value_,nature_value
 
 class Nature_pic():
     ''' Traitement du format d'une clause PIC 
@@ -96,23 +84,34 @@ class Nature_pic():
         >>> obj = Nature_pic('999')
         >>> obj.nature
         'NUM'
+        >>> obj.longueur
+        3
         >>> obj = Nature_pic('S999')
         >>> obj.nature
         'SNUM'
+        >>> obj.longueur
+        3
         >>> obj.virgule
         False
         >>> obj = Nature_pic('99V9')
         >>> obj.nature
         'FLOAT'
+        >>> obj.longueur
+        3
+        >>> obj.longueur
+        3
         >>> obj = Nature_pic('S99V9')
         >>> obj.nature
         'SFLOAT'
+        >>> obj.longueur
+        3
         >>> obj.virgule
         True
         >>> obj = Nature_pic('XX')
         >>> obj.nature
         'ALN'
-
+        >>> obj.longueur
+        2
 
     '''
     support = {'X' :'ALN' , '9' :'NUM', 'S' :'SNUM' ,'A' :'ALP'}
@@ -121,18 +120,40 @@ class Nature_pic():
         self.pic = pic
         self.nature = self.support.get(self.pic[0])
         if not self.nature :
-            raise('FORMAT NON SUPPORTE' , pic)
+            raise Exception(f"FORMAT Picture NON SUPPORTE: {pic}")
         else:
             self.pose_virgule()
+            self.calcul_longueur()
 
     def pose_virgule(self):
         if self.nature[-3:] == 'NUM' and 'V' in self.pic :
             self.virgule = True
             self.nature = self.nature.replace('NUM', 'FLOAT')
         else:
-            self.virgule = False 
+            self.virgule = False
 
-                    
+    def calcul_longueur(self):
+        tabv = re.findall(r'\(\d+\)', self.pic)
+        long = -1
+        flag_par = 0
+        if tabv:
+            for i, item in enumerate(tabv):
+                item = item.translate(str.maketrans({'(':'' ,')':'' }))
+                tabv[i]  = item
+                flag_par = 1
+            long = int(tabv[0])    
+        else :
+            long = len(self.pic)
+        if self.pic[0] == 'S' and flag_par == 0:
+            long -= 1
+        if self.virgule : 
+            if flag_par == 0 :
+                long -= 1
+            else:    
+                tab_ = re.search(r'V(9+)' ,self.pic)
+                if tab_:
+                    long += len(tab_[1])
+        self.longueur = long
  
 
 
@@ -236,6 +257,7 @@ class ZoneIndependante(Zone):
         (type_,pic, longueur) =  Zone.traite_pic(tab)
         longueur_interne  = longueur_externe = longueur
         (valeur, nature) = Zone.traite_value(tab) 
+        
         return cls(tab[1] ,type_, longueur_interne,  longueur_interne, pic, valeur , nature)
 
 
@@ -244,5 +266,5 @@ if __name__ == '__main__':
     import doctest
     doctest.run_docstring_examples(ZoneIndependante.from_ligne,None, verbose = False)
     doctest.run_docstring_examples(Nature_pic,None, verbose = False)
-
+    doctest.run_docstring_examples(Zone.traite_value,None, verbose = False)
     print("fin des tests internes")
