b9f54661c08b691710513a764b84f776e3d43a9e
Eric German <german.eric@gmail.com>
2022-10-10 22:50:26 +0200

    comportement aln

--- pycobol/comportement.py
+++ pycobol/comportement.py
@@ -1,9 +1,38 @@
 from dataclasses import dataclass, field
 
 @dataclass
-class Comportement()
+class Comportement():
     type: str
     longueur: int
+    valeur: str
     valeur_externe : str
     nature_valeur :str
 
+@dataclass
+class Comportement_aln(Comportement):
+    ''' Permet de cadrer les variable de type ALN 
+        >>> obj = Comportement_aln('ALN' , 10 , None, None , None)
+        >>> obj.initialize()
+        >>> obj.valeur_externe
+        '          '
+        >>> obj = Comportement_aln('ALN' , 10 , 'eric', None , None)
+        >>> obj.initialize()
+        >>> obj.valeur_externe
+        'eric      '
+        >>> len(obj.valeur_externe)
+        10
+    '''
+    def __init__(self, *args ):
+        self.cadrage = 'LEFT'
+        super().__init__(*args)
+
+    def initialize(self):
+        if self.valeur == None :
+            self.valeur_externe = ' ' * self.longueur
+        else: 
+            self.valeur_externe = self.valeur
+            self.valeur_externe = self.valeur.ljust(self.longueur,' ')    
+        
+if __name__ == '__main__':  
+    import doctest          
+    doctest.run_docstring_examples(Comportement_aln,None, verbose = False)
