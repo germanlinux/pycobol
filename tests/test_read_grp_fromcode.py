da3c33b85f459fe92d851e67e976032477c31ae7
Eric German <german.eric@gmail.com>
2022-11-06 11:24:49 +0100

    Create test_read_grp_fromcode.py

---
+++ tests/test_read_grp_fromcode.py
@@ -0,0 +1,23 @@
+zg1 ='''
+10            WW04-DAECH.                                   
+      11            WW04-AECH   PICTURE  9(4).                 
+      11            WW04-MECH   PICTURE  99.                      
+      11            WW04-JECH   PICTURE  99.                 
+'''
+zg2 ='''
+10                  OC00.                                  
+      11            OC00-CODS  PICTURE  X.               
+      11            OC00-CODAP PICTURE  X(3).            
+      11            OC00-COAS  PICTURE  X(6).            
+      11            OC00-NPRO  PICTURE  XXX.             
+      11            OC00-CODE  PICTURE  X.               
+      11            OC00-COCOM PICTURE  9(3).            
+      11            OC00-LIGNE  PICTURE  X(13).   
+'''
+zg3= '''
+10            OA00-IDNCN.                              
+      11            OA00-NUPSI  PICTURE  X(15).              
+      11            OA00-CNAEM  PICTURE  9(5).               
+      11            OA00-NUPEN  PICTURE  X(9). 
+'''  
+    
