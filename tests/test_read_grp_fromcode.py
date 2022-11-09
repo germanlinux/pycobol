7e1f4d4a63e0fb2535404d3b834422b3db082c0c
Eric German <german.eric@gmail.com>
2022-11-09 08:15:56 +0100

    Update test_read_grp_fromcode.py

--- tests/test_read_grp_fromcode.py
+++ tests/test_read_grp_fromcode.py
@@ -5,13 +5,13 @@ import unittest
 
 
 
-zg1 ='''
+zg1 = '''
 10            WW04-DAECH.                                   
       11            WW04-AECH   PICTURE  9(4).                 
       11            WW04-MECH   PICTURE  99.                      
       11            WW04-JECH   PICTURE  99.                 
 '''
-zg2 ='''
+zg2 = '''
 10                  OC00.                                  
       11            OC00-CODS  PICTURE  X.               
       11            OC00-CODAP PICTURE  X(3).            
@@ -21,15 +21,32 @@ zg2 ='''
       11            OC00-COCOM PICTURE  9(3).            
       11            OC00-LIGNE  PICTURE  X(13).   
 '''
-zg3= '''
+zg3 = '''
 10            OA00-IDNCN.                              
       11            OA00-NUPSI  PICTURE  X(15).              
       11            OA00-CNAEM  PICTURE  9(5).               
       11            OA00-NUPEN  PICTURE  X(9). 
 '''  
+
+zg4 = '''
+10            OA00-IDNCN.                              
+      11            OA00-NUPSI  PICTURE  X(15). 
+      11            DEUXIEME.
+      12            OA00-CNAEM  PICTURE  9(5).               
+      12            OA00-NUPEN  PICTURE  X(9). 
+'''  
+
+
 class TestZonegroupeline(unittest.TestCase):
       def test_lecture_groupe1(self):
             for cp, extrait in enumerate([zg1, zg2, zg3]):
                   tlignes = zn.ZoneGroupe.fake_read_file(extrait)
                   zn.ZoneGroupe.read_groupe_from_code(tlignes)
-                  self.assertEqual (len(zn.ZoneGroupe.zone_groupe[cp].fils),len(tlignes) - 1) 
+                  self.assertEqual (len(zn.ZoneGroupe.zone_groupe[cp].fils),len(tlignes) - 1)
+
+      def test_lecture_groupe2(self):
+                  tlignes = zn.ZoneGroupe.fake_read_file(zg4)
+                  zn.ZoneGroupe.read_groupe_from_code(tlignes)
+                  self.assertEqual (len(zn.ZoneGroupe.zone_groupe[0].fils),len(tlignes) - 1) 
+                  self.assertEqual (len(zn.ZoneGroupe.zone_groupe[1].fils),len(tlignes) - 1) 
+
