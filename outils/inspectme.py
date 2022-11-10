db75a77d991aa7e30f2fb8cb4533127480ac59c9
Eric German <german.eric@gmail.com>
2022-11-10 14:45:08 +0100

    Create inspectme.py

---
+++ outils/inspectme.py
@@ -0,0 +1,12 @@
+import sys
+print(sys.path)
+sys.path.insert(0, "../pycobol")
+from pycobol import zonegroupe as zngp
+from pycobol import comportement as cp
+from pycobol import zonage as zn 
+from inspect import getmembers
+listem = getmembers(zn.Zone)
+for item in listem:
+    if '__' not in item[0]:
+        print(type(item) , item)
+
