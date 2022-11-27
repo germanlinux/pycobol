af50ca35cd46d40a6694ec29b5065e88c374ec91
Eric German <german.eric@gmail.com>
2022-11-27 21:45:07 +0100

    Update test_step1.py

--- tests/test_step1.py
+++ tests/test_step1.py
@@ -1,44 +1,37 @@
 from pycobol import zonegroupe as zn 
 from pycobol import comportement as cp
-
+import sys
+sys.path.insert(0, "../pycobol")
 import unittest  
 
 class TestSteptest(unittest.TestCase):
     def test_aln1(self):    
-        obj_ = zn.ZoneGroupe('essai',1, 0)
-        self.assertEqual (obj_.fils,[])
+        obj2 = zn.ZoneIndependante( 'MADONNEE', 'X(5)')
+        obj2.move_value('12')
+        self.assertEqual (obj2.valeur_externe,'12   ')
+
+    def test_aln2(self):    
+        obj2 = zn.ZoneIndependante( 'MADONNEE', 'X(5)')
+        obj2.move_value(12)
+        self.assertEqual (obj2.valeur_externe,'12   ')
+
+    def test_aln3(self):    
+        obj2 = zn.ZoneIndependante( 'MANUM', '9(5)')
+        obj2.move_value('eg')
+        self.assertEqual (obj2.valeur_externe,'000eg')
+
+    def test_aln4(self):    
+        obj2 = zn.ZoneIndependante( 'MANUM', '9(5)')
+        obj2.move_value('e')
+        self.assertEqual (obj2.valeur_externe,'0000e')
     
-    def test_recurs1(self):
-        obj_ = zn.ZoneGroupe('essai',1, 0)
-        objfils = zn.ZoneGroupe('zonegrpfils', 2)
-        obj_.ajout_fils_groupe(objfils)
-        objfilsimp = zn.ZoneFilsSimple('essaifils', 5, picture = '999')
-        obj_.ajout_fils_simple(objfilsimp)
-        objfilsimp2 = zn.ZoneFilsSimple('essaifilssp', 5, picture = 'X(5)')
-        objfils.ajout_fils_simple(objfilsimp2)
-        obj_.maj_valeur()
-        self.assertEqual (obj_.valeur_externe,"     000")
+    def test_aln5(self):    
+        obj2 = zn.ZoneIndependante( 'MANUM', '9(5)')
+        obj2.move_value('12')
+        self.assertEqual (obj2.valeur_externe,'00012')
 
-    def test_recurs2(self):
-        obj_ = zn.ZoneGroupe('essai',1, 0)
-        objfilsimp = zn.ZoneFilsSimple('essaifils', 5, picture = '999')
-        objfilsimp2 = zn.ZoneFilsSimple('essaifilssp', 5, picture = 'X(5)')
-        objfilsimp3 = zn.ZoneFilsSimple('essaifils', 5, picture = '999')
-        obj_.ajout_fils_simple(objfilsimp)
-        obj_.ajout_fils_simple(objfilsimp2)
-        obj_.ajout_fils_simple(objfilsimp3)
-        obj_.init_groupe()
-        self.assertEqual (obj_.longueur_utile,11)    
-        obj_.move_value('ERIC')
-        self.assertEqual(obj_.valeur_externe,'ERIC       ')
-        self.assertEqual(objfilsimp.valeur_externe,'ERI')
-        self.assertEqual(objfilsimp2.valeur_externe,'C    ')
-        self.assertEqual(objfilsimp3.valeur_externe,'   ')
-        self.assertEqual(objfilsimp3.longueur_utile,3)
-
-        obj_.move_value('tresgrandtropgrand')
-        self.assertEqual(obj_.valeur_externe,'tresgrandtr')
-        self.assertEqual(objfilsimp.valeur_externe,'tre')
-        self.assertEqual(objfilsimp2.valeur_externe,'sgran')
-        self.assertEqual(objfilsimp3.valeur_externe,'dtr')
-        self.assertEqual(objfilsimp3.longueur_utile,3)
+    def test_aln6(self):    
+        obj2 = zn.ZoneIndependante( 'MANUM', '9(5)')
+        obj2.move_value(12)
+        self.assertEqual (obj2.valeur_externe,'00012')
+    
