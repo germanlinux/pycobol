f4cd3f9c300a8510f74e7d83d1c4f12ed52fe0b5
Eric German <german.eric@gmail.com>
2022-12-03 08:28:28 +0100

    Update test_redefine.py

--- tests/test_redefine.py
+++ tests/test_redefine.py
@@ -22,6 +22,19 @@ class Testredefine(unittest.TestCase):
         self.assertEqual (obj2.valeur_externe,'00012')
         self.assertEqual (objr.valeur_externe,'00012')   
 
+    def test_aln3(self):    
+        obj_ = zn.ZoneGroupe('essai',1, 0)
+        objfilsimp = zs.ZoneFilsSimple('essaifils', 5, picture = '999')
+        objfilsimp2 = zs.ZoneFilsSimple('essaifilssp', 5, picture = 'X(5)')
+        objfilsimp3 = zs.ZoneFilsSimple('essaifils', 5, picture = '999')
+        obj_.ajout_fils_simple(objfilsimp)
+        obj_.ajout_fils_simple(objfilsimp2)
+        obj_.ajout_fils_simple(objfilsimp3)
+        objr = zs.ZoneSimpleRedefine('essaifils','REDEF1' ,5, picture = 'X(3)')
+        obj_.init_groupe() 
+        obj_.move_value('123ERICG456')
+        print(objfilsimp3.valeur_externe)
+        print(objfilsimp2.valeur_externe)
 
 
 if __name__ == '__main__':
