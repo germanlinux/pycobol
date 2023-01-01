c16b5926284abce5827ad17d22e5d7cae1e62047
Eric German <german.eric@gmail.com>
2023-01-01 18:18:05 +0100

    Update etiquette.py

--- pycobol/etiquette.py
+++ pycobol/etiquette.py
@@ -3,7 +3,15 @@ from dataclasses import dataclass, field
 @dataclass
 class Etiquette():
     ''' Cette classe est destinée a définir des etiquettes comme dans les programmes COBOL
-    avec la notion de paragraphe
+    avec la notion de paragraphe.
+
     '''
     nom: str
     instructions: list = []
+
+@dataclass
+class  Instruction():
+    ''' Une instruction est un objet, une méthode et un ou plusieur parametres
+    '''
+    pass
+    
