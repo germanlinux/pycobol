e48650ffb569b6720ce01e8838b600a43c4c4bf9
Eric German <german.eric@gmail.com>
2023-01-07 09:21:02 +0100

    Update etiquette.py

--- pycobol/etiquette.py
+++ pycobol/etiquette.py
@@ -15,7 +15,7 @@ class  Instruction():
     ''' Une instruction est un objet, une méthode et un ou plusieur parametres
     '''
     
-    def display(self, *args):
+    def display(self, liste):
         ''' le format de qqchose :
         Une chaine de caractere ?  "eric"
         Un tuple ?: (nomcobol,"chaine", nomcobol2" ) etc 
@@ -27,21 +27,21 @@ class  Instruction():
         >>> data1.valeur_externe
         '00012'
         >>> a = Instruction()
-        >>> a.display("essai")
+        >>> a.display(["essai"])
         essai
-        >>> a.display(data1)
+        >>> a.display([data1])
         00012
-        >>> a.display(data1,' essai' )
+        >>> a.display([data1,' essai'] )
         00012 essai
         '''
 
-        qqchose = list(args)
+        qqchose = tup
         chaine = ""
         for item in qqchose:
                 if type(item) == str :
                     chaine += item
-        else:
-                chaine += item.valeur_externe
+                else:
+                    chaine += item.valeur_externe
         print(chaine)            
         return None
 
