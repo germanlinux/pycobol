e7f8470b998f0da0854bb7d00e9adeccbb5d2fa9
Eric German <german.eric@gmail.com>
2023-01-26 09:03:36 +0100

    Update test_etiquette.py

--- tests/test_etiquette.py
+++ tests/test_etiquette.py
@@ -1,13 +1,14 @@
 import sys
 sys.path.insert(0, "../pycobol")
-from pycobol import arbrezone as arb
+#from pycobol import arbrezone as arb
 from unittest.mock import Mock 
-
+from pycobol import execution as exe
 import unittest  
 
 class Testprogram(unittest.TestCase):
     def test_p1(self):
-        pass
+        pgm = exe.Program('demo')
+        self.assertEqual(pgm.vidage(),'Etiquette:Debut_programme\n')
         
 if __name__ == '__main__':
     unittest.main()
