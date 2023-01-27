2efc85f4e4d1ebac3b44b585d69f08a8062df67c
Eric German <german.eric@gmail.com>
2023-01-27 16:55:10 +0100

    Update test_etiquette.py

--- tests/test_etiquette.py
+++ tests/test_etiquette.py
@@ -9,6 +9,13 @@ class Testprogram(unittest.TestCase):
     def test_p1(self):
         pgm = exe.Program('demo')
         self.assertEqual(pgm.vidage(),'Etiquette:Debut_programme\n')
-        
+
+    def test_p1(self):
+        pgm = exe.Program('demo')
+        inst = exe.Instruction('display', ['hello world'])
+        pgm.add_step(inst) 
+        self.assertEqual(len(pgm.vidage()),46)
+
+ 
 if __name__ == '__main__':
     unittest.main()
