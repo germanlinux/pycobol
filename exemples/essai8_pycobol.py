8030ff16905e957dd4f1427b586089e0254e1c66
Eric German <german.eric@gmail.com>
2023-02-07 18:04:06 +0100

    Update essai8_pycobol.py

--- exemples/essai8_pycobol.py
+++ exemples/essai8_pycobol.py
@@ -2,7 +2,11 @@
 import sys
 sys.path.insert(0, "../pycobol")
 from pycobol import parser_cobol
-lignes_cobol = parser_cobol.fake_read_file_proc()
+zg1 ='''        PROCEDURE DIVISION.
+                            DISPLAY "Hello world !".
+                            STOP RUN.'''
+
+lignes_cobol = parser_cobol.fake_read_file_proc(zg1)
 pgm = parser_cobol.load_procedure(lignes_cobol)
 print(pgm.vidage())
 pgm.run()
