bbcdbf5a817d4afb6a732c640ea08ae1d79be78a
Eric German <german.eric@gmail.com>
2023-04-04 11:13:59 +0200

    Update test_stepmove.py

--- tests/test_stepmove.py
+++ tests/test_stepmove.py
@@ -1,37 +1,33 @@
-from pycobol import zonage as zn 
-from pycobol import comportement as cp
 import sys
 sys.path.insert(0, "../pycobol")
 import unittest  
+from pycobol import parser_cobol
+from pycobol import execution
 
-class TestSteptest(unittest.TestCase):
+
+class TestStepMove(unittest.TestCase):
+
+    def setUp(self):
+        zg1 ='''        PROCEDURE DIVISION.
+                            DISPLAY "Hello world !".'''
+        zg2 ='''
+                77    NOM PIC X(10).
+                77    PRENOM PIC X(10).'''  
+        tlignes = parser_cobol.fake_read_file(zg2)
+        arbre =parser_cobol.read_groupe_from_code(tlignes)
+        arbre.autonomme(globals())
+        lignes_cobol = parser_cobol.fake_read_file_proc(zg1)
+        self.pgm = parser_cobol.load_procedure(lignes_cobol)
+        print('OK')
+
     def test_aln1(self):    
-        obj2 = zn.ZoneIndependante( 'MADONNEE', 'X(5)')
-        obj2.move_value('12')
-        self.assertEqual (obj2.valeur_externe,'12   ')
+        _inst = execution.Instruction('move', ["ERIC", _prenom])
+        self.pgm.add_step(_inst)
+        _inst = execution.Instruction('move', [1234, _nom])
+        self.pgm.add_step(_inst)
+        self.pgm.run()
+        self.assertEqual(_nom.valeur_externe,'1234      ')
+        self.assertEqual(_prenom.valeur_externe,'ERIC      ')
 
-    def test_aln2(self):    
-        obj2 = zn.ZoneIndependante( 'MADONNEE', 'X(5)')
-        obj2.move_value(12)
-        self.assertEqual (obj2.valeur_externe,'12   ')
-
-    def test_aln3(self):    
-        obj2 = zn.ZoneIndependante( 'MANUM', '9(5)')
-        obj2.move_value('eg')
-        self.assertEqual (obj2.valeur_externe,'000eg')
-
-    def test_aln4(self):    
-        obj2 = zn.ZoneIndependante( 'MANUM', '9(5)')
-        obj2.move_value('e')
-        self.assertEqual (obj2.valeur_externe,'0000e')
-    
-    def test_aln5(self):    
-        obj2 = zn.ZoneIndependante( 'MANUM', '9(5)')
-        obj2.move_value('12')
-        self.assertEqual (obj2.valeur_externe,'00012')
-
-    def test_aln6(self):    
-        obj2 = zn.ZoneIndependante( 'MANUM', '9(5)')
-        obj2.move_value(12)
-        self.assertEqual (obj2.valeur_externe,'00012')
-    
+if __name__ == '__main__':
+    unittest.main()        
