65bafda7cc6c895c95715e991bb628859e2bdd19
Eric German <german.eric@gmail.com>
2023-04-08 09:45:34 +0200

    test vscodium

--- pycobol/controls.py
+++ pycobol/controls.py
@@ -13,5 +13,6 @@ def controle_compatible(a, b):
         return True
     if 'NUM' in a.son_type and 'GRP' in b.son_type:
         return True
+        
     ## TODO tester la suite
     return False
