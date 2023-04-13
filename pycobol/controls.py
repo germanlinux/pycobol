94354e3e8e9c03bbd890b0fedc1de1ef4d6fe2a9
Eric German <german.eric@gmail.com>
2023-04-13 21:10:57 +0200

    control

--- pycobol/controls.py
+++ pycobol/controls.py
@@ -1,3 +1,6 @@
+class Constant:
+    pass
+
 def controle_compatible(e, r):
     ''' cette fonction controle la compatibilité de nature des zones
     pour un move'''
@@ -6,15 +9,18 @@ def controle_compatible(e, r):
     except: 
         ## c est un type de base
         ## lequel ? : int, float , str ?
-        
+        if type(e) == str :
+            e_ = Constant()
+            e_.son_type ='ALN'
+            e = e_
 
-    if 'FLOAT' in a.son_type and 'FLOAT' in b.son_type:
+    if 'FLOAT' in e.son_type and 'FLOAT' in b.son_type:
         return True
-    if 'ALN' in a.son_type and 'ALN' in b.son_type:
+    if 'ALN' in e.son_type and 'ALN' in b.son_type:
         return True
-    if 'NUM' in a.son_type and 'NUM' in b.son_type:
+    if 'NUM' in e.son_type and 'NUM' in b.son_type:
         return True
-    if 'GRP' in a.son_type and 'GRP' in b.son_type:
+    if 'GRP' in e.son_type and 'GRP' in b.son_type:
         return True    
     if 'ALN' in a.son_type and 'GRP' in b.son_type:
         return True
